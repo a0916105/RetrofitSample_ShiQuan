97f5d32bffdd9154bdedd902aa54c84f4f1bf1f6
a0916105 <whdofjew@gmail.com>
2021-05-01 14:37:08 +0800

    Service Interface

---
+++ app/src/main/java/tw/idv/jew/retrofitsample/GithubService.kt
@@ -0,0 +1,15 @@
+package tw.idv.jew.retrofitsample
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GithubService {
+    @GET("users/{user}/repos")
+    fun listRepos(
+        @Path("user") user: String,
+        @Query("type") type: String? = null,
+        @Query("sort") sort: String? = null
+    ): Call<List<Repo>>
+}
